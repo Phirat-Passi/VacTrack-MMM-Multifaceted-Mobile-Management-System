8dd32ab99bed8a9d05f657832be56f0801e77953
Phirat <67471222+Phirat-Passi@users.noreply.github.com>
2021-05-17 18:38:08 +0530

    Create en.php

---
+++ IZ Gateways/en.php
@@ -0,0 +1,53 @@
+<?php
+/**************************************************
+* PluginLotto.com                                 *
+* Copyrights (c) 2005-2010. iZAP                  *
+* All rights reserved                             *
+***************************************************
+* @author iZAP Team "<support@izap.in>"
+* @link http://www.izap.in/
+* @version {version} $Revision: {revision}
+* Under this agreement, No one has rights to sell this script further.
+* For more information. Contact "Tarun Jangra<tarun@izap.in>"
+* For discussion about corresponding plugins, visit http://www.pluginlotto.com/pg/forums/
+* Follow us on http://facebook.com/PluginLotto and http://twitter.com/PluginLotto
+*/
+
+add_translation('en', array(
+  'izap_payment:no_gateway_found' => 'No payment gateway found, Please contact site administration',
+  'izap_payment:undefined_gateway' => 'Undefined gateway',
+  'izap_payment:file_not_find' => 'File not found',
+  'izap_payment:class_not_found' => 'Unable to load the payment gateway',
+  'izap_payment:empty_param' => 'Unable to set the params, might be empty.',
+  'izap_payment:choose_gateway' => 'Choose payment gateway',
+  'izap_payment:gateways_settings' => 'Settings',
+  'izap_payment:method_not_supported' => 'This payment method is not supported yet',
+
+  'izap_payment:loading' => 'Loading ....',
+
+  'izap_payment:choose_multiple' => 'Choose multiple',
+  'izap_payment:choose_single' => 'Choose single',
+  'izap_payment:bypass_payment' => 'By pass payment option',
+  
+  'izap_payment:paypal_account' => 'Paypal account',
+  'izap_payment:test_mode' => 'Test mode',
+  'izap_payment:yes' => 'Yes',
+  'izap_payment:no' => 'No',
+  'izap_payment:submit' => 'Save',
+
+  'izap_payment:payleap_user_id' => 'User login id',
+  'izap_payment:payleap_user_pass' => 'User login password',
+  'izap_payment:card_type' => 'Card type',
+  'izap_payment:card_number' => 'Card number',
+  'izap_payment:exp_date' => 'Card expire date (mm/yy)',
+  'izap_payment:card_cvv' => 'Card CVV number',
+
+  'izap_payment:authorize_login_key' => 'Authorize login key',
+  'izap_payment:authorize_authorization_key' => 'Authorize authorization key',
+
+  'izap_payment:alertpay_user_id' => 'Alertpay username',
+  'izap_payment:alertpay_IPN_security_code' => 'IPN Security Code',
+  'izap_payment:alert_url' => 'Your alert url',
+
+  'izap_payment:help' => 'Help',
+));
